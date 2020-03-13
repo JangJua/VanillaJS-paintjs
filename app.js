9910ec0ec4a609aebcad6b2925f080047c967d57
JangJua <jua0610@naver.com>
2020-03-14 02:47:22 +0900

    drawing path

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 400;
+canvas.height = 400;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
 function stopPainting() {
     painting = false;
 }
-function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
-}
 
-function  onMouseDown(event) {
+function  startPainting() {
     painting = true;
 }
 
-function onMouseUp(event) {
-    stopPainting();
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath(); //create a new path
+        ctx.moveTo(x, y); //moves the starting point of the path
+    } else {
+        ctx.lineTo(x, y); //Connects the last point of the path to (x, y)
+        ctx.stroke();
+    }
 }
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting)
 }
