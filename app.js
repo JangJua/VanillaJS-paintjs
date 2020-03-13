49be80c238370727e8bc92e614ddbd24bf37c948
JangJua <jua0610@naver.com>
2020-03-14 03:25:23 +0900

    handle changing color

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
-canvas.width = 400;
+canvas.width = 800;
 canvas.height = 400;
 
 ctx.strokeStyle = "#2c2c2c";
@@ -29,9 +30,17 @@ function onMouseMove(event) {
     }
 }
 
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting)
 }
+
+Array.from(colors).forEach(color =>
+    color.addEventListener("click", handleColorClick));
